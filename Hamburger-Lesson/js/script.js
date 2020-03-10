a419e97fa47b5477c5689275eabb2925f601ef45
sekine11 <sekine11665@gmail.com>
2020-03-10 15:56:57 +0900

    [Finish] 6-10

--- Hamburger-Lesson/js/script.js
+++ Hamburger-Lesson/js/script.js
@@ -1,10 +1,9 @@
 $(function() {
  
-  $('#back a').on('click',function(){
-    $('body, html').animate({
-      scrollTop:0
-    }, 800);
-      return false;
+  $('.menu-trigger').on('click', function() {
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle();
+    return false;
   });
  
-});
+ });
